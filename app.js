328428bbca0ae79e9dde17429733c2c2d3c2b518
line0921 <64320652+line0921@users.noreply.github.com>
2021-09-07 17:01:04 -0500

    ContactMe page

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+// import express from "express"
+// import cors from "cors"
+
+// import router from "./routes/router.js"
+
 function showNav(){
     document.getElementsByClassName("navigation")[0].classList.toggle("active");
 }
+
+
+
+// const app = express()
+
+// app.use(express.json())
+// app.use(cors())
+// app.use(router);
+// app.use("/", express.static("public"))
+// const port = 3000;
+
+
+// app.get("/hello", (request,response) => {
+//     response.status(200).send("hello world");
+// });
+
+// app.listen(port,() =>{
+//     console.log("port running on http://localhost:" + port)
+// }); 
